41908aacfc744f70ea7c8c6dcfacebac2ad045d0
CarlosHSF <44176623+CarlosHSF99@users.noreply.github.com>
2023-05-03 23:00:28 +0100

    Implement ShippingCompany class (#11)

--- src/main/java/model/ShippingCompany.java
+++ src/main/java/model/ShippingCompany.java
@@ -1,16 +1,37 @@
 package model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShippingCompany {
-    public ShippingCompany() {
+    private BigDecimal baseValue;
+    private BigDecimal fee;
+    private BigDecimal revenue;
+    private final List<Order> orders;
+
+    public ShippingCompany(BigDecimal baseValue, BigDecimal fee) {
+        this.baseValue = baseValue;
+        this.fee = fee;
+        this.revenue = BigDecimal.ZERO;
+        this.orders = new ArrayList<>();
     }
 
-    public ShippingCompany(ShippingCompany other) {
+    private ShippingCompany(ShippingCompany other) {
+        this.baseValue = other.baseValue;
+        this.fee = other.fee;
+        this.revenue = other.revenue;
+        this.orders = other.orders.stream().map(Order::clone).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order.clone());
+        revenue = revenue.add(shippingCost(order.getProducts().size()));
     }
 
     public BigDecimal shippingCost(long count) {
-        return null;
+        return baseValue.multiply(BigDecimal.ONE.add(fee));
     }
 
     public ShippingCompany clone() {

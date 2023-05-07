3a22e7be1ec3cab0133984c8291597e609b189d1
Carlos Ferreira <carloshsf3899@gmail.com>
2023-05-07 19:22:05 +0100

    Make Order calculations greedy

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -15,35 +15,49 @@ public class Order {
     private static final BigDecimal USED_FEE = new BigDecimal("0.25");
     private static long numberOfProducts = 0;
     private final String id;
+    private final String buyerId;
+    private final String sellerId;
+    private final String shippingCompanyId;
     private final Map<String, Product> products;
     private final LocalDateTime creationDate;
-    private final String sellerId;
-    private final String buyerId;
-    private final ShippingCompany shippingCompany;
+    private final BigDecimal productsCost;
+    private final BigDecimal shippingCost;
+    private final BigDecimal vintageFees;
+    private final BigDecimal totalCost;
     private LocalDateTime deliveryDateTime;
     private Status status;
 
-    public Order(Collection<Product> products, String sellerId, String buyerId, ShippingCompany shippingCompany) {
+    public Order(Collection<Product> products, String buyerId, String sellerId, String shippingCompanyId, BigDecimal shippingCost) {
         this.id = nextAlphanumericId();
-        this.products = products.stream().collect(Collectors.toMap(Product::getId, Function.identity()));
-        this.sellerId = sellerId;
         this.buyerId = buyerId;
-        this.shippingCompany = shippingCompany.clone();
+        this.sellerId = sellerId;
+        this.shippingCompanyId = shippingCompanyId;
+        this.products = products.stream().collect(Collectors.toMap(Product::getId, Function.identity()));
         this.creationDate = LocalDateTime.now();
+        this.productsCost = products.stream().map(Product::price).reduce(BigDecimal.ZERO, BigDecimal::add);
+        this.shippingCost = shippingCost;
+        this.vintageFees = products.stream()
+                .map(product -> product.isNew() ? NEW_FEE : USED_FEE)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        this.totalCost = this.productsCost.add(this.shippingCost).add(vintageFees);
         this.status = Status.INITIALIZED;
     }
 
     private Order(Order other) {
         this.id = nextAlphanumericId();
+        this.buyerId = other.buyerId;
+        this.sellerId = other.sellerId;
+        this.shippingCompanyId = other.shippingCompanyId;
         this.products = other.products.entrySet()
                 .stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().clone()));
         this.creationDate = other.creationDate;
         this.deliveryDateTime = other.deliveryDateTime;
+        this.productsCost = other.productsCost;
+        this.shippingCost = other.shippingCost;
+        this.vintageFees = other.vintageFees;
+        this.totalCost = other.totalCost;
         this.status = other.status;
-        this.sellerId = other.sellerId;
-        this.shippingCompany = other.shippingCompany.clone();
-        this.buyerId = other.buyerId;
     }
 
     public String getId() {
@@ -82,8 +96,8 @@ public class Order {
         return status != Status.RETURNED;
     }
 
-    public ShippingCompany getShippingCompany() {
-        return shippingCompany;
+    public String getShippingCompanyId() {
+        return shippingCompanyId;
     }
 
     public void expedite() throws StatusOrderException {
@@ -111,30 +125,24 @@ public class Order {
         status = Status.RETURNED;
     }
 
-    public BigDecimal finalCost() {
-        return productsCost().add(shippingCost()).add(vintageFees());
+    public BigDecimal totalCost() {
+        return totalCost;
     }
 
     public BigDecimal productsCost() {
-        return products.values().stream().map(Product::price).reduce(BigDecimal.ZERO, BigDecimal::add);
+        return productsCost;
     }
 
     public BigDecimal shippingCost() {
-        return shippingCompany.shippingCost(products.size());
+        return shippingCost;
     }
 
     public BigDecimal vintageFees() {
-        return products.values()
-                .stream()
-                .map(product -> product.isNew() ? NEW_FEE : USED_FEE)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return vintageFees;
     }
 
     public BigDecimal sellerRevenue() {
-        return status != Status.RETURNED ? products.values()
-                .stream()
-                .map(Product::price)
-                .reduce(BigDecimal.ZERO, BigDecimal::add) : BigDecimal.ZERO;
+        return status != Status.RETURNED ? productsCost : BigDecimal.ZERO;
     }
 
     private String nextAlphanumericId() {
@@ -144,12 +152,18 @@ public class Order {
     @Override
     public String toString() {
         return "Order{" +
-                "products=" + products +
+                "id='" + id + '\'' +
+                ", sellerId='" + sellerId + '\'' +
+                ", buyerId='" + buyerId + '\'' +
+                ", shippingCompanyId='" + shippingCompanyId + '\'' +
+                ", products=" + products +
                 ", creationDate=" + creationDate +
+                ", productsCost=" + productsCost +
+                ", shippingCost=" + shippingCost +
+                ", vintageFees=" + vintageFees +
+                ", totalCost=" + totalCost +
                 ", deliveryDateTime=" + deliveryDateTime +
                 ", status=" + status +
-                ", sellerCode='" + sellerId + '\'' +
-                ", shippingCompany=" + shippingCompany +
                 '}';
     }
 
@@ -160,23 +174,35 @@ public class Order {
 
         Order order = (Order) o;
 
+        if (!id.equals(order.id)) return false;
+        if (!sellerId.equals(order.sellerId)) return false;
+        if (!buyerId.equals(order.buyerId)) return false;
+        if (!shippingCompanyId.equals(order.shippingCompanyId)) return false;
         if (!products.equals(order.products)) return false;
         if (!creationDate.equals(order.creationDate)) return false;
+        if (!productsCost.equals(order.productsCost)) return false;
+        if (!shippingCost.equals(order.shippingCost)) return false;
+        if (!vintageFees.equals(order.vintageFees)) return false;
+        if (!totalCost.equals(order.totalCost)) return false;
         if (!Objects.equals(deliveryDateTime, order.deliveryDateTime))
             return false;
-        if (status != order.status) return false;
-        if (!sellerId.equals(order.sellerId)) return false;
-        return shippingCompany.equals(order.shippingCompany);
+        return status == order.status;
     }
 
     @Override
     public int hashCode() {
-        int result = products.hashCode();
+        int result = id.hashCode();
+        result = 31 * result + sellerId.hashCode();
+        result = 31 * result + buyerId.hashCode();
+        result = 31 * result + shippingCompanyId.hashCode();
+        result = 31 * result + products.hashCode();
         result = 31 * result + creationDate.hashCode();
+        result = 31 * result + productsCost.hashCode();
+        result = 31 * result + shippingCost.hashCode();
+        result = 31 * result + vintageFees.hashCode();
+        result = 31 * result + totalCost.hashCode();
         result = 31 * result + (deliveryDateTime != null ? deliveryDateTime.hashCode() : 0);
         result = 31 * result + status.hashCode();
-        result = 31 * result + sellerId.hashCode();
-        result = 31 * result + shippingCompany.hashCode();
         return result;
     }
 
